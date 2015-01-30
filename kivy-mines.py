723bf32264f0ee96090b31b4087eaa5c2d5043c9
Barbaros Yıldırım <barbaros.yildirim@markafoni.com>
2015-01-30 13:23:14 +0200

    pep8 entegration, board button class used.

--- kivy-mines.py
+++ kivy-mines.py
@@ -1,4 +1,3 @@
-
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, FadeTransition, WipeTransition, RiseInTransition, FallOutTransition
 from kivy.uix.button import Button
@@ -8,61 +7,75 @@ from kivy.utils import get_color_from_hex
 from kivy.properties import (NumericProperty, ListProperty)
 from mine import Mine
 
-class kivyMines(ScreenManager):
-	board = ListProperty()
-	horizontal = NumericProperty()
-	vertical = NumericProperty()
 
-	def hover(self, *args):
-		hover = get_color_from_hex('ACACAC')
-		normal = get_color_from_hex('E2DDD5')
+class BoardButton(Button):
+    def __init__(self, hidden, image=None, *args, **kwargs):
+        super(BoardButton, self).__init__(*args, **kwargs)
+        self.hidden = hidden
+        self.image = image
 
-		mouse_position = args[1]
-		if self.current == "board_selection":
-			obj = self.board_selection
-		else:
-			obj = self.board_screen.board
 
-		for but in obj.children:
-			x1, x2 = but.pos[0], but.pos[0] + but.height
-			y1, y2 = but.pos[1], but.pos[1] + but.width
-			if x1 < mouse_position[0] < x2 and \
-				y1 < mouse_position[1] < y2:
-				but.background_color = hover
-			else:
-				but.background_color = normal
-		# 	print but.text, x1, x2, '-', y1, y2
-		# print mouse_position
-		# print ""
+class KivyMines(ScreenManager):
+    board = ListProperty()
+    horizontal = NumericProperty()
+    vertical = NumericProperty()
 
-	def switch_screen(self, screen):
-		self.transition = WipeTransition()
-		self.current = screen
+    def hover(self, *args):
+        hover = get_color_from_hex('ACACAC')
+        normal = get_color_from_hex('E2DDD5')
 
-	def board_select(self, *args):
-		self.horizontal, self.vertical = map(int, args)
-		mine = Mine(self.horizontal, self.vertical)
-		self.board = map(int, mine.board.reshape(1, self.horizontal*self.vertical)[0])
-		self.switch_screen(screen='board_screen')
+        mouse_position = args[1]
+        if self.current == "board_selection":
+            obj = self.board_selection
+        else:
+            obj = self.board_screen.board
 
-		self.current_screen.board.clear_widgets()
-		for cell in self.board:
-			button = Button(text="[color=000000]%s[/color]"%cell)
-			self.current_screen.board.add_widget(button)
+        for but in obj.children:
+            x1, x2 = but.pos[0], but.pos[0] + but.height
+            y1, y2 = but.pos[1], but.pos[1] + but.width
+            if x1 < mouse_position[0] < x2 and \
+                                    y1 < mouse_position[1] < y2:
+                but.background_color = hover
+            else:
+                but.background_color = normal
+                # print but.text, x1, x2, '-', y1, y2
+                # print mouse_position
+                # print ""
 
-class kivyMinesApp(App):
-	
-	def __init__(self, *args, **kwargs):
-		super(kivyMinesApp, self).__init__(*args, **kwargs)
-		Builder.load_file('assets/mines.kv')
-		self.title = 'Kivy Mines'
-		self.icon = 'assets/mine.png'
+    def board_click(self, *args):
+        button = args[0]
+        print button.hidden
 
-	def build(self):
-		mines = kivyMines()
-		Window.bind(mouse_pos=mines.hover)
-		return mines
+    def switch_screen(self, screen):
+        self.transition = WipeTransition()
+        self.current = screen
+
+    def board_select(self, *args):
+        self.horizontal, self.vertical = map(int, args)
+        mine = Mine(self.horizontal, self.vertical)
+        self.board = map(int, mine.board.reshape(1, self.horizontal * self.vertical)[0])
+        self.switch_screen(screen='board_screen')
+
+        self.current_screen.board.clear_widgets()
+        for cell in self.board:
+            button = BoardButton(text="[color=000000]%s[/color]"%cell, hidden=cell)
+            button.bind(on_press=self.board_click)
+            self.current_screen.board.add_widget(button)
+
+
+class KivyMinesApp(App):
+    def __init__(self, *args, **kwargs):
+        super(KivyMinesApp, self).__init__(*args, **kwargs)
+        Builder.load_file('assets/mines.kv')
+        self.title = 'Kivy Mines'
+        self.icon = 'assets/mine.png'
+
+    def build(self):
+        mines = KivyMines()
+        Window.bind(mouse_pos=mines.hover)
+        return mines
+
 
 if __name__ == '__main__':
-	Window.clearcolor = (get_color_from_hex('F0F0F0'))
-	kivyMinesApp().run()
+    Window.clearcolor = (get_color_from_hex('F0F0F0'))
+    KivyMinesApp().run()
--- mine.py
+++ mine.py
@@ -1,82 +1,76 @@
 from numpy import zeros
-from copy import copy
 from random import randint
 
 
 class Mine(object):
-	def __init__(self, horizontal=16, vertical=16):
-		self.horizontal = horizontal
-		self.vertical = vertical
-		self.board = zeros(self.horizontal*self.vertical)
-		self.bomb_count = self.horizontal*self.vertical/5
-		# Prepare the Board.
-		self.locate_bombs()
-		self.reshape_board()
-		self.point_neighbours()
+    def __init__(self, horizontal=16, vertical=16):
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.board = zeros(self.horizontal * self.vertical)
+        self.bomb_count = self.horizontal * self.vertical / 5
+        # Prepare the Board.
+        self.locate_bombs()
+        self.reshape_board()
+        self.point_neighbours()
 
+    def as_str(self):
+        """
+        To see the board view on terminal or any other viewable screen
+        """
+        ss = ""
+        for line in self.board:
+            ss += "\t".join(map(str, map(int, line))) + "\n\v"
+        return ss
 
-	def as_str(self):
-		"""
-		To see the board view on terminal or any other viewable screen
-		"""
-		ss = ""
-		for line in self.board:
-			ss += "\t".join(map(str, map(int, line))) + "\n\v"
-		return ss
+    def locate_bombs(self):
+        """
+        Randomly find bomb positions on board bomb count calculated at the beginning
+        positions signed with the value of random index with '-1'
+        if the count of these signed positions are reached the calculated bomb count,
+        iterations will be stopped. The key point is, with little posibility,
+        same index should be passed by this way this problem will be eliminated.
+        """
+        while len(filter(lambda x: x == -1, self.board)) < self.bomb_count:
+            i = randint(0, (self.horizontal * self.vertical) - 1)
+            self.board[i] = -1
 
+    def reshape_board(self):
+        """
+        Board should be shaped with wanted horizontal and vertical count.
+        """
+        self.board = self.board.reshape(self.horizontal, self.vertical)
 
-	def locate_bombs(self):
-		"""
-		Randomly find bomb positions on board bomb count calculated at the beginning
-		positions signed with the value of random index with '-1'
-		if the count of these signed positions are reached the calculated bomb count, 
-		iterations will be stopped. The key point is, with little posibility, 
-		same index should be passed by this way this problem will be eliminated.
-		"""
-		while len(filter(lambda x: x == -1, self.board)) < self.bomb_count:
-		    i = randint(0, (self.horizontal*self.vertical)-1)
-		    self.board[i] = -1
+    def point_neighbours(self):
+        """
+        Neighbours bomb count will be calculated.
+        """
+        line_index = 0
+        for line in self.board:
+            col_index = 0
+            for col in line:
+                if col == -1:
+                    top_left = line_index - 1, col_index - 1
+                    top = line_index - 1, col_index
+                    top_right = line_index - 1, col_index + 1
 
+                    left = line_index, col_index - 1
+                    right = line_index, col_index + 1
 
-	def reshape_board(self):
-		"""
-		Board should be shaped with wanted horizontal and vertical count.
-		"""
-		self.board = self.board.reshape(self.horizontal, self.vertical)
+                    bot_left = line_index + 1, col_index - 1
+                    bot = line_index + 1, col_index
+                    bot_right = line_index + 1, col_index + 1
 
+                    positions = [top_left, top, top_right,
+                                 left, right,
+                                 bot_left, bot, bot_right]
 
+                    for line_pos, col_pos in positions:
+                        try:
+                            if self.board[line_pos][col_pos] > -1:
+                                self.board[line_pos][col_pos] += 1
+                        except IndexError:
+                            pass
 
-	def point_neighbours(self):
-		"""
-		Neighbours bomb count will be calculated.
-		"""
-		line_index = 0
-		for line in self.board:
-			col_index = 0
-			for col in line:
-				if col == -1:
-					top_left = line_index-1, col_index -1
-					top = line_index-1, col_index
-					top_right = line_index-1, col_index+1
-
-					left = line_index, col_index-1
-					right  = line_index, col_index+1
-
-					bot_left = line_index+1, col_index-1
-					bot = line_index+1, col_index
-					bot_right = line_index+1, col_index+1
-
-					positions = [top_left, top, top_right,
-								 left, right,
-								 bot_left, bot, bot_right]
-
-					for line_pos, col_pos in positions:
-						try:
-							if self.board[line_pos][col_pos] > -1:
-								self.board[line_pos][col_pos] += 1
-						except IndexError:
-							pass
-
-				col_index += 1
-			line_index += 1
-		pass
+                col_index += 1
+            line_index += 1
+        pass
